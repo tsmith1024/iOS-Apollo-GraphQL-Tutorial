26935d865b3035f9722149236f1710f7ab88044b
Taylor <tsmith1024@gmail.com>
2023-11-02 14:30:09 -0500

    Initial Commit

---
+++ GraphQL Tutorial/GraphQL_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQL_TutorialApp.swift
+//  GraphQL Tutorial
+//
+//  Created by Taylor Smith on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GraphQL_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
